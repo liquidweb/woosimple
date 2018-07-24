feb576a41bbfc7dfc3bd16c6d31847a9a2650283
Steve Grunwell <sgrunwell@liquidweb.com>
2018-07-24 14:10:47 +0000

    Be consistent in the values being used for the woosimple-toggle-switch
    All of the logic has been counting on a boolean (with the meta value itself being stored as either a '1' or '0'), while some of the recent updates used "on" to indicate a truthy-state. However, type-juggling within PHP would interpret both "on" and "off" as truth-y, causing the value to be evaluated in unexpected ways.
    
    Fixes #5.

--- inc/product-edit.php
+++ inc/product-edit.php
@@ -81,7 +81,7 @@ function render_toggle_metabox() {
 
 	<p>
 		<label>
-			<input name="woosimple-toggle-switch" value="on" id="woosimple-toggle-switch" class="woosimple-toggle-switch" type="checkbox" <?php checked( 'on', $active ); ?>>
+			<input name="woosimple-toggle-switch" id="woosimple-toggle-switch" class="woosimple-toggle-switch" type="checkbox" <?php checked( true, $active ); ?>>
 			<?php esc_html_e( 'Simplify this Page', 'woosimple' ); ?>
 		</label>
 	</p>
